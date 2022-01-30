b1214ec7fa553e940928488bfe219911782ecc51
Mohit <94623283+ErElectrical@users.noreply.github.com>
2022-01-30 06:34:53 +0000

    Added : Main class Of Application

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ProductReviewManagement
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Product Review Mangement");
+            AddToDataTable data = new AddToDataTable();
+            data.AddToTable();
+        }
+    }
+
+
+}
